10b6b0df250a0e7e96c8160e335d4cffc2266394
tiba666 <frehr@hotmail.com>
2019-02-20 11:31:59 +0100

    first commit

---
+++ Assets/Scripts/Charector/HP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HP : MonoBehaviour
+{
+
+    public int hp = 100;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Bullet")
+        {
+            hp -= 10;
+        }
+    }
+}
